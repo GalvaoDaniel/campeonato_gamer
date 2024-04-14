9246758bd3925fa45b8b535fa5daf211b03941be
Daniel Galvão <galvaoazevedod@gmail.com>
2024-04-14 00:35:28 -0300

    Implementação do filtro de dados da listagem

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,24 +1,37 @@
-import { Avatar, Box, HStack, Heading, ScrollView, StatusBar, VStack , Text, Spacer, Input } from "native-base";
 import { FlatList, ImageBackground, View } from "react-native";
-import Competidor from "../../model/competidor";
 import headerIMG from "../../assets/control_image.jpg";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { styles } from './style';
 import { Card, CardProps } from '../../components/card';
 import { SearchInput } from "../../components/searchInput";
+import { Controller, useForm } from 'react-hook-form';
 
 type Props = {
     navigation: any
   }
 
+  type SearchFormProps = {
+    nome: string
+  }
+
 export const Home = ({ navigation }: Props) => {
+
+    const { control, register, getValues, watch }  = useForm<SearchFormProps>({});
+    const watchCep = watch("nome");
     const [data, setData] = useState<CardProps[]>([]);
+    const [handledData, setHandledData] = useState<CardProps[]>([]);
 
     useFocusEffect(useCallback(() => {
         handlerFetchData()
     }, []))
+
+    useEffect(() => {
+        if (getValues("nome").length == 0) {
+            setHandledData(data)
+        }
+    }, [watch])
     
     function handleEdit(id: string) {
         navigation.navigate('Competidor', {id: id})
@@ -30,6 +43,7 @@ export const Home = ({ navigation }: Props) => {
             const jsonValue = await AsyncStorage.getItem('@crud_form:competidor')
             const data = jsonValue ? JSON.parse(jsonValue) : [];
             setData(data);
+            setHandledData(data);
             return jsonValue
         }catch(error) {
             console.log(error);
@@ -37,26 +51,45 @@ export const Home = ({ navigation }: Props) => {
     }
 
     const handleFilter = async () => {
-        // try {
-        //     const filteredCeps = newCeps.filter(
-        //         (cep) => cep !== undefined
-        //     ) as CepModel[];
-        //     setCeps(filteredCeps);
-        // } catch (error) {
-        //     console.error('Error fetching data: ', error)
-        // }
-        console.log("Ainda não implementado")
+        const nomePesquisa = getValues("nome");
+        console.log("Entrou");
+        console.log("handledData: " + handledData);
+        if (nomePesquisa.length > 1) {
+
+            setHandledData(data);
+            console.log("handledData 2 : " + handledData);
+            const newHandledData = handledData.filter(
+                (competidor) => competidor.primeiroNome.includes(nomePesquisa)
+            );
+            setHandledData(newHandledData);
+        } else if (nomePesquisa.length == 0) {
+            setHandledData(data);
+        }
     }
 
     return (
         <>
             <ImageBackground source={headerIMG} >
-                <SearchInput variant="round" placeholder="Pesquisar..." onPress={handleFilter}/>
+                <Controller 
+                    control={control}
+                    name="nome"
+                    defaultValue=''
+                    render={({field: {onChange, value}})=>(
+                        <SearchInput 
+                            variant="round" 
+                            placeholder="Pesquisar..." 
+                            onPress={handleFilter} 
+                            value={value} 
+                            onChangeText={onChange} 
+                            {...register("nome")}
+                        />
+                    )}
+                />
             </ImageBackground>
 
             <View style={styles.container}>
                 <FlatList 
-                    data={data}
+                    data={handledData}
                     keyExtractor={item=>item.id}
                     style={styles.list}
                     contentContainerStyle={styles.listContent}
